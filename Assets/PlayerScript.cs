f6534dadf4de668b2aae7da984a2edacde8d02a4
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-02-25 22:01:35 +0900

    new

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Transform target;
+	public Transform[] targets;  
+	public NavMeshAgent agent;
+	public int currentTarget = 0;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update () {
+		agent.SetDestination (target.position);
+
+		if (Vector3.Distance(transform.position, target.position) < 1) {
+			if (currentTarget < targets.Length - 1) {
+				currentTarget += 1;
+			}
+			else {
+				currentTarget = 0;
+			}
+		}
+
+		target = targets[currentTarget];
+
+		if (target != null) {
+			agent.SetDestination (target.position);
+		}
+	}
+
+	void OnCollisionEnter(){
+		print ("OK");
+	}
+}
